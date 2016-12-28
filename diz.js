78e5bc378dfd9baabf461d6063e4a544bb7a2213
nju33 <nju33.ki@gmail.com>
2016-12-29 01:38:58 +0900

    Export file split, directory post plugin

--- diz.js
+++ diz.js
@@ -1,4 +1,1 @@
-exports.Diz = require('diz-theme-minimalist');
-exports.Directory = require('./dist/directory').default;
-exports.Post = require('./dist/post').default;
-exports.Plugin = require('./dist/plugin').default;
+module.exports = require('diz-theme-minimalist');
